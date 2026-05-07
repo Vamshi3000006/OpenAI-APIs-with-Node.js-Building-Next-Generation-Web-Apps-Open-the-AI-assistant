b169ed9dfa4b85f7cc86f27419e6d95834fd2f9d
Your Name <you@example.com>
2026-05-07 18:45:06 -0500

    Implemented Chat_completion_stream function and displayed the output on to web page

--- controllers/chatCompletionController.js
+++ controllers/chatCompletionController.js
@@ -1,22 +1,42 @@
-const openai = require('../config/openaiConfig')
+const openai = require('../config/openaiConfig');
 
-const generateChatCompletion = async (user_role_content) => {
-  system_role_content = "You reply as concisely as possible. If you are not sure about an answer, you will respond with \" I don't know\"."
-  
-  messages = [
-    { 'role': 'system', 'content': system_role_content},
-    { 'role': 'user', 'content': user_role_content}
-  ]
+const generateChatCompletion = async (req, res) => {
+  const system_role_content = "You reply as concisely as possible. If you are not sure about an answer, you will respond with \"I don't know\".";
+
+  const messages = [
+    { role: 'system', content: system_role_content },
+    { role: 'user', content: req.body.user_role_content }
+  ];
+
   const chatCompletion = await openai.chat.completions.create({
     messages: messages,
     model: 'gpt-3.5-turbo',
     temperature: 0.5,
     max_tokens: 512
   });
-  // console.log(chatCompletion.choices[0].message.content);
-  // console.log(chatCompletion);
-  console.log(chatCompletion.choices[0].message);
+
+  res.status(200).json({
+    content: chatCompletion.choices[0].message.content
+  });
+};
+
+const generateChatCompletionStream = async (req, res ) => {
+
+  messages = [{
+    'role': 'user', 'content': req.query.prompt
+  }];
+
+  const stream = await openai.chat.completions.create({
+    model: "gpt-4-turbo",
+    messages: messages,
+    stream: true,
+  });
+
+  for await (const chunk of stream) {
+    res.write(chunk.choices[0]?.delta?.content || "");
+  }
+
+  res.end();
 }
 
-
-module.exports = { generateChatCompletion }
+module.exports = { generateChatCompletion,generateChatCompletionStream };
--- playground.js
+++ playground.js
@@ -1,16 +1,34 @@
-// console.log("hello openai Apis");
-
 const readline = require('readline');
-const { generateChatCompletion } = require('./controllers/chatCompletionController');
+const { generateChatCompletion,generateChatCompletionStream } = require('./controllers/chatCompletionController');
 
 const r1 = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-////////////// CHAT COMPLETION - START ////////////////////
+// r1.question("Enter Prompt to send for GPT3.5 ChatCompletion Model: \n", 
+//   (user_role_content) => {
+//     const req = {
+//       body: {
+//         user_role_content: user_role_content
+//       }
+//     };
+
+//     const res = {
+//       status: function(statusCode) {
+//         return {
+//           json: function(data) {
+//             console.log(data.content);
+//             r1.close();
+//           }
+//         };
+//       }
+//     };
+
+//     generateChatCompletion(req, res);
+//   }
+// );
+
 r1.question("Enter Prompt to send for GPT3.5 ChatCompletion Model: \n", 
-  (user_role_content) => {
-    generateChatCompletion(user_role_content)
-  });
-////////////// CHAT COMPLETION - END //////////////////////
+  (user_role_content) => generateChatCompletionStream(user_role_content)
+) 
